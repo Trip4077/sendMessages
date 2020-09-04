8fe55e59bbf34e43887a4a98f2b7fce0d2767a14
Trip4077 <bj2336@email.vccs.edu>
2020-09-04 01:16:33 -0400

    Project MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import SendTags from './SendTags';
 
+import './App.css';
+
 function App() {
+  const [recipientsList, updateRecipientsList] = useState("");
+
+  useEffect(() => {
+    axios.get( process.env.REACT_APP_API_KEY )
+         .then( res => { updateRecipientsList( res.data ) })
+         .catch( err => { console.dir(err) });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header" style={{backgroundColor: "#153D5C"}}>
       <img alt="nr_beta_logo" 
           src="https://noterouter-staging.firebaseapp.com/static/media/logo.b10c9223.png" 
           style={{width: '30%'}} />
-        <SendTags />
+        <SendTags recipientsList={ recipientsList } />
       </header>
     </div>
   );
--- src/SendTags.js
+++ src/SendTags.js
@@ -1,34 +1,79 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-export default function SendTags () {
-    const [recipients, updateRecipients] = useState("")
-    const [qualifier, updateQualifier] = useState("")
-    const [sendTo, updateSendTo] = useState("")
-    const [sendType, updateSendType] = useState("")
-    const [sent, updateSent] = useState(false)
+export default function SendTags (props) {
+    const [recipients, updateRecipients] = useState("");
+    const [qualifier, updateQualifier] = useState("AND");
+    const [sendTo, updateSendTo] = useState("");
+    const [sendType, updateSendType] = useState("organizationId");
+    const [sent, updateSent] = useState(false);
 
-    const handleChange = (event) => {
-        const value = event.target.value
+    const matchAND = (recipient, matchList) => {
+        // Must match all terms in matchList to return recipient
+        const matchGoal = matchList.length;
+        let matches = 0;
+
+        for(let i = 0; i < matchGoal; i++) {
+            if( recipient[ sendType ].toLowerCase().includes( matchList[i].toLowerCase() ) ) matches++;
+        }
+
+        if( matches === matchGoal ) return `${recipient.firstName} ${recipient.lastName}`;
+    }
+
+    const matchOR = (recipient, matchList) => {
+        // Must match single term in matchList to return recipient
+        for(let i = 0; i < matchList.length; i++) {
+            if( recipient[ sendType ].toLowerCase().includes( matchList[i].toLowerCase() )) {
+                return `${recipient.firstName} ${recipient.lastName}`;
+            }
+        }
+    }
+    
+    const handleChange = event => {
+        const value = event.target.value;
+
         switch(event.target.name) {
             case "sendType":
-                updateSendType(value)
+                updateSendType(value);
                 return
             case "sendTo":
-                updateSendTo(value)
+                updateSendTo(value);
                 return
             case "qualifier":
-                updateQualifier(value)
+                updateQualifier(value);
                 return
             default:
                 return;
         }
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = event => {
         event.preventDefault()
         /*  implement me
             hint: we will probably need to update state here to render the right parts
         */
+       const matchList = sendTo.split(',');
+
+       // Filter list of all recipients down to input matches
+       const filteredResult = props.recipientsList.map( recipient => { 
+           switch( qualifier ) {
+               case "AND":
+                  return matchAND( recipient, matchList );
+               case "OR":
+                  return matchOR( recipient, matchList );
+               default:
+                   return;
+           }
+       })
+       .filter( recipient => recipient !== undefined );
+
+       // Determine if any recipients were found and update sent state accordingly
+       if( filteredResult.length ) {
+          updateRecipients( filteredResult.join(', ') );
+          updateSent( true );
+       } else {
+          updateRecipients( filteredResult.join(', ') );
+          updateSent( false );
+       }
     }
 
     return (
@@ -36,21 +81,29 @@ export default function SendTags () {
             <form onSubmit={handleSubmit} style={{textAlign: "left"}}>
                 <label style={{paddingRight: "10px"}}>
                     <div>
-                        <span style={{paddingRight: "10px"}}>Send Type (Organization, First Name, Last Name, or Tags):</span>
-                        <input type="text" name="sendType" onChange={handleChange}/>
+                        <span style={{ paddingRight: "10px" }}>Send Type:</span>
+                        <select name="sendType" onChange={handleChange}>
+                            <option value="organizationId">Organization</option>
+                            <option value="firstName">First Name</option>
+                            <option value="lastName">Last Name</option>
+                            <option value="tags">Tags</option>
+                        </select>
                     </div>
                     <div>
-                        <span style={{paddingRight: "10px", paddingTop: "20px"}}>Send To (separated by commas):</span>
+                        <span style={{ paddingRight: "10px" }}>Send To (separated by commas):</span>
                         <input type="text" name="sendTo" onChange={handleChange}/>
                     </div>
                     <div>
-                        <span style={{paddingRight: "10px", paddingTop: "20px"}}>AND/OR?: </span>
-                        <input type="text" name="qualifier" onChange={handleChange}/>
+                        <span style={{ paddingRight: "10px" }}>AND/OR?: </span>
+                        <select name="qualifier" onChange={handleChange}>
+                            <option value="AND">And</option>
+                            <option value="OR">Or</option>
+                        </select>
                     </div>
                 </label>
                 <input type="submit" value="Send Messages" />
             </form>
-            { sent && <div>Sent to: {recipients}</div> }
+            { sent && <div>Sent to: {typeof recipients === "string" ? recipients : "Type Error"}</div> }
         </div>
     )
 }
